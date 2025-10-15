47a021bf444f3c215f767f483a58ce5adcec2a63
uzzz0119 <1191596158@qq.com>
2025-10-15 22:22:51 +0800

    remove ignored files

--- push.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-git add -A
-git commit -m "chore: clean up temporary scripts and update README"
-git push origin master
-echo.
-echo Done! Check: https://github.com/uzzz0119/SayIt
-pause
-
