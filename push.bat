aa4b4dd86e2385b44c6d0809e927f68db1a785ce
uzzz0119 <1191596158@qq.com>
2025-10-15 20:42:48 +0800

    feat: update SayIt platform with Faster-Whisper and LLM features

---
+++ push.bat
@@ -0,0 +1,8 @@
+@echo off
+git add -A
+git commit -m "chore: clean up temporary scripts and update README"
+git push origin master
+echo.
+echo Done! Check: https://github.com/uzzz0119/SayIt
+pause
+
